eefec91c02f43ce79b362ad6fd5a861958aa8546
hana <kanounhana6@gmail.com>
2025-05-01 15:53:17 +0100

    front

--- src/main/java/com/example/scolarite/ProjetApplication.java
+++ src/main/java/com/example/scolarite/ProjetApplication.java
@@ -1,11 +1,18 @@
 package com.example.scolarite;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.scolarite.entities.Utilisateur;
+import com.example.scolarite.entities.Utilisateur.Role;
+import com.example.scolarite.services.IUtilisateurService;
+
 @SpringBootApplication
 public class ProjetApplication implements CommandLineRunner{
+	@Autowired
+	IUtilisateurService us;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetApplication.class, args);
@@ -14,7 +21,13 @@ public class ProjetApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		Utilisateur etudiant = new Utilisateur();
+        etudiant.setUsername("essra_hmida");
+        etudiant.setPassword("essra"); 
+        etudiant.setEmail("essra@email.com");
+        etudiant.setRole(Role.ETUDIANT);
+        us.creerUtilisateur(etudiant);
+        
 		
 	}
 
